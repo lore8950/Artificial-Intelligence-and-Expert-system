9047b8fde4da95b0d9927c4c2a0d5f258cc721cc
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-07 18:01:40 +0600

    Add basic arithmetic operations in main.py

---
+++ day01/main.py
@@ -0,0 +1,12 @@
+#Take two numbers from user and print sum, subtraction, multiplication
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_result = num1 + num2
+subtract_result = num1 - num2
+multiply_result = num1 * num2
+
+print(f"Sum: {sum_result}")
+print(f"Subtraction: {subtract_result}")
+print(f"Multiplication: {multiply_result}")
