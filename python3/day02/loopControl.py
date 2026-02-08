67fb70b86b5a0870ce6b4232295abfad34ed35ab
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-08 21:25:01 +0600

    Create loopControl.py

---
+++ python3/day02/loopControl.py
@@ -0,0 +1,16 @@
+#loop control
+for i in range(1, 10):
+  if i == 5:
+    break
+  print(i)
+
+  #print 1-5
+  for i in range(1, 10):
+    if i == 5:
+      break
+
+#print 1-4
+for i in range(1, 10):
+  if i == 5:
+    break
+  print(i)
