59ff0127cb1fe0bce24b5bce460417206fab9f09
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-08 21:42:50 +0600

    Create list_Operation.py

---
+++ python3/day02/list_Operation.py
@@ -0,0 +1,12 @@
+subject_marks = [85, 90, 78, 92, 88]
+
+print(subject_marks)
+print(subject_marks[1])
+
+subject_marks.append(95)
+print(subject_marks)
+
+subject_marks.remove(78)
+print(subject_marks)
+
+print(len(subject_marks))
