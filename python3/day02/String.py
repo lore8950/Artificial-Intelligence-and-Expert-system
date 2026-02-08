8edf7c1ad458e7f53536637466930db3e6739495
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-08 21:36:33 +0600

    Create String.py

---
+++ python3/day02/String.py
@@ -0,0 +1,13 @@
+#String (Meaning of code)
+
+name = "Python programming"
+
+print(name)
+print(name[0])
+print(name[0:6])
+print(name[-1])
+
+#string methods
+print(name.upper())
+print(name.lower())
+print(len(name))
