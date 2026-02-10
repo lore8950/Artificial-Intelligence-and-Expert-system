b9b2567e71cee03fcf1f0707c95ae774ec2287ca
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-11 02:01:14 +0600

    Create random_number_generator.py

---
+++ python3/day04/random_number_generator.py
@@ -0,0 +1,9 @@
+# Import random using alias
+# Generate a random number between 1 and 10
+# Import only sqrt from math
+
+import random as rd
+#from math import sqrt
+
+print (rd.randint(1,20))
+#print (sqrt(49)
