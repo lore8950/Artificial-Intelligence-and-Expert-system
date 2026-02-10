42bb1bcc1ebc18872e244b64bdbd3d74753dbb81
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-10 20:29:04 +0600

    Create list_basic.py

---
+++ python3/day03/list_basic.py
@@ -0,0 +1,24 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+# Accessing elements
+print(my_list[0])
+print(my_list[-1])
+
+# Slicing
+print(my_list[1:4])
+
+# Modifying elements
+my_list[0] = 10
+print(my_list)
+
+# Adding elements
+my_list.append(6)
+print(my_list)
+
+# Removing elements
+my_list.remove(3)
+print(my_list)
+
+# Length of the list
+print(len(my_list))
