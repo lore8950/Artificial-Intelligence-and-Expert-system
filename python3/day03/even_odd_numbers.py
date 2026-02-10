20c7bb9898dcb1c9d13b897849525111642a4dda
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-10 20:31:44 +0600

    Create even_odd_numbers.py

---
+++ python3/day03/even_odd_numbers.py
@@ -0,0 +1,9 @@
+# Print even numbers from 1 to 20
+for i in range(1, 21):
+  if i % 2 == 0:
+    print("Even number:", i)
+
+# Print odd numbers from 1 to 20
+for i in range(1, 21):
+  if i % 2 != 0:
+    print("Odd number:", i)
