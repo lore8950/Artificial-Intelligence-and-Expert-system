0727a3511cde72b28f0837183223e41805ebbaa8
Apurbo Debnath <118726269+lore8950@users.noreply.github.com>
2026-02-11 01:28:50 +0600

    Create math_module_calculator.py

---
+++ python3/day03/math_module_calculator.py
@@ -0,0 +1,12 @@
+# Import the math module
+#Calculetor
+# Square root of 64
+#Factorial of 5
+# Value on π
+
+import math
+print (math.sqrt(64))
+print (math.factorial(5))
+print (math.pi)
+print (math.comb(10, 5))
+print (math.gcd(10,5))
